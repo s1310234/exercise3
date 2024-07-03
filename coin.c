805da99ec1d2c5810c146635bbe8e8c6913167db
s1310234 <ayu1019ayu@gmail.com>
2024-07-04 02:34:59 +0900

    Victory message added

--- coin.c
+++ coin.c
@@ -10,8 +10,17 @@ char* tossCoin() {
 int main() {
     srand(time(NULL));
 
+    char user_name[100]; // ユーザー名を格納するための変数
+
+    printf("Who are you?\n> ");
+    fgets(user_name, sizeof(user_name), stdin);
+    user_name[strcspn(user_name, "\n")] = '\0';  // 改行文字を削除する
+
+    printf("Hello, %s!\n", user_name); // ユーザー名で挨拶する
+
     printf("Tossing a coin...\n");
 
+    // コイン投げのシミュレーション
     char* results[3];
     int heads_count = 0, tails_count = 0;
 
@@ -26,6 +35,12 @@ int main() {
     }
 
     printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
+
+    if (heads_count > tails_count) {
+        printf("%s won!\n", user_name);
+    } else {
+        printf("%s lost!\n", user_name);
+    }
 
     return 0;
 }
