6416f9d654333e395c26ec952b5c9c4a7dd0d9d9
s1310234 <ayu1019ayu@gmail.com>
2024-07-04 01:50:29 +0900

    User name added

--- coin.c
+++ coin.c
@@ -1,8 +1,16 @@
+git checkout -b user_name
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main() {
+    char name[50];
+    printf("Who are you?\n> ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello, %s!\n", name);
+
     srand(time(NULL));
     int heads = 0, tails = 0;
 
@@ -23,3 +31,4 @@ int main() {
 
     return 0;
 }
+
